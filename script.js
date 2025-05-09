8071dc6d9640dcbac8eb7050c3dd69b8d24e61e6
davidkershaw1 <dkershaw27@belmontschools.net>
2025-05-09 11:08:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,254 @@
+
+
+function sports_100(){
+  let answer = "soccer"
+  let guess= prompt("Most popular sport in the world?")
+  if (guess == answer ){
+    alert ("Correct! +100")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+
+function sports_200(){
+    let answer = "894"
+  let guess= prompt("The NHL goal record before Alex Ovechkin broke it")
+  if (guess == answer ){
+    alert ("Correct! +200")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function sports_300(){
+  let answer = "barry bond"
+  let guess= prompt(" MLB player widely known as the face of the steroid era and the all time home run leader")
+  if (guess == answer ){
+    alert ("Correct! +300")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function sports_400(){
+  let answer = "drew brees"
+  let guess= prompt("This player holds second place on the all time passing yards record only behind Tom Brady")
+  if (guess == answer ){
+    alert ("Correct! +400")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function sports_500(){
+   let answer = "1967"
+  let guess= prompt("the year the toronto maple leafs last won a stanley cup")
+  if (guess == answer ){
+    alert ("Correct! +500")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+
+
+function geography_100(){
+  let answer = "texas"
+  let guess= prompt("This U.S. state has the nickname Lone Star State")
+  if (guess == answer ){
+    alert ("Correct! +100")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+
+function geography_200(){
+ let answer = "nile river"
+  let guess= prompt(" this african river is by far the longest in the world")
+  if (guess == answer ){
+    alert ("Correct! +200")
+  }
+  else{
+    alert("Incorrect!")
+  }}
+function geography_300(){
+   let answer = "superior, ontario, eerie, michigan, huron"
+  let guess= prompt(" these 5 lakes make up the great lakes (seperate by comas)")
+  if (guess == answer ){
+    alert ("Correct! +300")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function geography_400(){
+   let answer = "st. petersburg"
+  let guess= prompt(" this city was the capital of russia before Vladimir Lenin moved it to Moscow")
+  if (guess == answer ){
+    alert ("Correct! +400")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function geography_500(){
+  let answer = "brazil"
+  let guess= prompt("This nation shares the longest border with france despite not being in europe")
+  if (guess == answer ){
+    alert ("Correct! +500")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function presidents_100(){
+  let answer = "thomas jefferson"
+  let guess= prompt("Which president is most well known for writing the declaration of independence")
+  if (guess == answer ){
+    alert ("Correct! +100")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function presidents_200(){
+ let answer = "washington, jefferson, roosevelt, lincoln"
+  let guess= prompt("These 4 presidents have their faces carved into mount rushmore (use last names only seperated by comas)")
+  if (guess == answer ){
+    alert ("Correct! +200")
+  }
+  else{
+    alert("Incorrect!")
+  }}
+function presidents_300(){
+ let answer = "ulysses s grant"
+  let guess= prompt("This president is pictured on the 50 dollar bill")
+  if (guess == answer ){
+    alert ("Correct! +300")
+  }
+  else{
+    alert("Incorrect!")
+  } 
+}
+function presidents_400(){
+   let answer = "4"
+  let guess= prompt("The amount of presidents who called Massachusetts home")
+  if (guess == answer ){
+    alert ("Correct! +400")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function presidents_500(){
+   let answer = "JFK"
+  let guess= prompt("This president has their picture on the relatively unknown half dollar coin")
+  if (guess == answer ){
+    alert ("Correct! +500")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function tvshows_100(){
+   let answer = "office"
+  let guess= prompt("Taking place in Scranton, PA, this show follows the dunder mifflin paper company")
+  if (guess == answer ){
+    alert ("Correct! +100")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function tvshows_200(){
+   let answer = "better call saul"
+  let guess= prompt("A spinoff of another extremely popular show, this show follows a lawyer with an extremely memorable motto")
+  if (guess == answer ){
+    alert ("Correct! +200")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function tvshows_300(){
+   let answer = "the simpsons"
+  let guess= prompt("This cartoon has stood as the longest running animated show in America")
+  if (guess == answer ){
+    alert ("Correct! +300")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function tvshows_400(){
+    let answer = "south park"
+  let guess= prompt("Butters, Wendy, Craig, and Clyde are all side characters in this very edgy and popular cartoon")
+  if (guess == answer ){
+    alert ("Correct! +400")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function tvshows_500(){
+   let answer = "seinfeld"
+  let guess= prompt("Taking place on long island, this sitcom stars a comedian as himself in one of the most critically acclaimed shows of all time")
+  if (guess == answer ){
+    alert ("Correct! +500")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function movies_100(){
+   let answer = "oppenheimer"
+  let guess= prompt(" Actor Cillian Murphy won his first Oscar for best male performance with this historical nonfiction drama directed by Christopher Nolan")
+  if (guess == answer ){
+    alert ("Correct! +100")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function movies_200(){
+  let answer = "it"
+  let guess= prompt(" You’ll float to is the main catchphrase of the villain in this horror film")
+  if (guess == answer ){
+    alert ("Correct! +200")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function movies_300(){
+  let answer = "jaws"
+  let guess= prompt("After a major prop malfunction, Director Steven Spielberg turned to shadows and music to create suspense in this 1975 film")
+  if (guess == answer ){
+    alert ("Correct! +300")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function movies_400(){
+  let answer = "project x"
+  let guess= prompt("This 2011 film portrays an australian party that happened in real life to the big screen taking place in Pasadena California")
+  if (guess == answer ){
+    alert ("Correct! +400")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
+function movies_500(){
+  let answer = "good will hunting"
+  let guess= prompt("Matt Damon and Ben Affleck famously wrote the script for this movie for a filmmaking class at harvard about a janitor at MIT with an intelligence way higher than his job")
+  if (guess == answer ){
+    alert ("Correct! +500")
+  }
+  else{
+    alert("Incorrect!")
+  }
+}
